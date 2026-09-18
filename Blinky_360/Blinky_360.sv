22d05154481d84c90c993393ecae3114c82fbcd7
Connor Hoang <connorhoang3@gmail.com>
2026-09-17 22:16:01 -0400

    Fixed bug where else if merged if statements

--- Blinky_360/Blinky_360.sv
+++ Blinky_360/Blinky_360.sv
@@ -3,9 +3,9 @@
 module top (
     input  logic clk, 
     // output logic LED    
-    output logic RGB_R,
-    output logic RGB_G,
-    output logic RGB_B
+    output logic RGB_R = 0,
+    output logic RGB_G = 1,
+    output logic RGB_B = 1
 );
 
     // Clock every 0.5s -> change to be (1/6)s to get full cycle every second
@@ -19,46 +19,36 @@ module top (
         if (count == BLINK_INTERVAL - 1) begin
             count <= 0;
             if (color_idx == 5) begin
-                color_idx <= 0; // If we are at Magenta, loop back to Red
+                color_idx <= 0; // If Magenta, loop back to Red
             end
             else begin
                 color_idx <= color_idx + 1; // Otherwise, move to the next color
             end
             if (color_idx == 0) begin //// Is it more expensive to assign all three instead of just the changes, or is it negligible?
-                // set rgb to red
+                // set rgb to red -> useful reset in case somethings mucks up elsewhere, but probably unecessary
                 RGB_R <= 0;
                 RGB_G <= 1;
                 RGB_B <= 1;
             end
-            if (color_idx == 1) begin
+            else if (color_idx == 1) begin
                 // set rgb to yellow
-                RGB_R <= 0;
                 RGB_G <= 0;
-                RGB_B <= 1;
             end
-            if (color_idx == 2) begin
+            else if (color_idx == 2) begin
                 // set rgb to green
                 RGB_R <= 1;
-                RGB_G <= 0;
-                RGB_B <= 1;
             end
-            if (color_idx == 3) begin
+            else if (color_idx == 3) begin
                 // set rgb to cyan
-                RGB_R <= 1;
-                RGB_G <= 0;
                 RGB_B <= 0;
             end
-            if (color_idx == 4) begin
+            else if (color_idx == 4) begin
                 // set rgb to blue
-                RGB_R <= 1;
                 RGB_G <= 1;
-                RGB_B <= 0;
             end
-            if (color_idx == 5) begin
+            else if (color_idx == 5) begin
                 // set rgb to magenta
                 RGB_R <= 0;
-                RGB_G <= 1;
-                RGB_B <= 0;
             end
         end
         else begin
@@ -67,3 +57,5 @@ module top (
     end
 
 endmodule
+
+//// nX is the convention for paths? -> useful for debugging glitches?
